6e3f8d944efd280b596ed626b27d2e44008b9cc8
Marko Jocic <jocicmarko@gmail.com>
2016-01-02 12:54:33 +0100

    lab8-begin

---
+++ lab8-begin/src/main/webapp/static/js/app.js
@@ -0,0 +1,81 @@
+var wafepaApp = angular.module('wafepaApp', ['ngRoute']);
+
+wafepaApp.controller('ActivityController', function($scope, $http, $location, $routeParams) {
+	
+	$scope.getActivities = function() {
+		$http.get('api/activities')
+				.success(function(data) {
+					$scope.activities = data;
+					$scope.successMessage = 'Everything OK';
+				})
+				.error(function() {
+					$scope.errorMessage = 'Oops, something went wrong.';
+				});
+	};
+	
+	$scope.deleteActivity = function(id, index) {
+		$http.delete('api/activities/' + id)
+				.success(function() {
+					$scope.activities.splice(index, 1);
+				})
+				.error(function() {
+					
+				});
+	};
+	
+	$scope.initActivity = function() {
+		$scope.activity = {};
+		
+		if ($routeParams && $routeParams.id) {
+			$http.get('api/activities/' + $routeParams.id)
+					.success(function(data) {
+						$scope.activity = data;
+					})
+					.error(function() {
+						
+					});
+		}
+	};
+	
+	$scope.saveActivity = function() {
+		if ($scope.activity.id) {
+			$http.put('api/activities/' + $scope.activity.id, $scope.activity)
+					.success(function() {
+						$location.path('/activities');
+					})
+					.error(function() {
+						
+					});
+		} else {
+			$http.post('api/activities', $scope.activity)
+					.success(function() {
+						$location.path('/activities');
+					})
+					.error(function() {
+						
+					});
+		}
+	};
+});
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : 'static/html/home.html'
+        })
+        .when('/activities', {
+            templateUrl : 'static/html/activities.html',
+            controller : 'ActivityController'
+        })
+        .when('/activities/add', {
+            templateUrl : 'static/html/addEditActivity.html',
+            controller : 'ActivityController'
+        })
+        .when('/activities/edit/:id', {
+            templateUrl : 'static/html/addEditActivity.html',
+            controller : 'ActivityController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
